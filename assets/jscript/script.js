8d9a2e81ea275533de31ded17353f5a5c4bdfd14
loneWolf <chotansharma@yahoo.com>
2021-12-23 00:42:31 -0500

    done with comment

--- assets/jscript/script.js
+++ assets/jscript/script.js
@@ -9,6 +9,7 @@ var rangeOfPassword = function() {
         // recursive function to ensure a right input
         rangeOfPassword();
     }
+    // return the value of the function
     return range;
 }
 
@@ -24,8 +25,9 @@ var numbers = "0123456789";
 // a string variable for special character
 var specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-// now an array of strings of upperCase, lowerCase, numbers and SpecialChar for looping over
+// created an empty array 
 var arrayOfStrings =  [];
+// pushed the string variables inside it to create an array of strings
 arrayOfStrings.push(upperCase);
 arrayOfStrings.push(lowerCase);
 arrayOfStrings.push(numbers);
@@ -34,12 +36,13 @@ arrayOfStrings.push(specialChar);
 
 // the function for generate the password
 function generatePassword() {
-    // initialise an empty string to hold the return value
+    // initialised an empty string to add all the four strings of the array into a single string
     var passwordChar ="";
+    // initialised another empty string to hold the password string
     var finalPassword ="";
     // calling the rangeOfPassword() function for user's input
     var passwordLength = rangeOfPassword();
-    console.log(passwordLength);
+
     // confirming the uppercase
     var confirmUpper = window.confirm("Do you want the uppercase letter in your password?" );
     // confirming the lower case letters
@@ -56,17 +59,17 @@ function generatePassword() {
         window.alert("Sorry, you did not put valid inputs!");
         generatePassword();
     } else {
+        // looping over the string array
         for(var i =0; i < arrayOfStrings.length; i++) {
-            // test looping works
+            // added the value into the empty string
             passwordChar+=arrayOfStrings[i];
-            console.log(passwordChar);
         }
-
+        // looping over the string and use of Math.random() to get the randomly generated password
         for (var j = 0; j <= passwordLength; j++){
           finalPassword += passwordChar.charAt(Math.floor(Math.random() * passwordChar.length)); 
         }
     }
-
+    // return the value of the finalPassword
     return  finalPassword;
 };
 
